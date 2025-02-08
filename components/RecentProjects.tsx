7158e088944c022f63207b77dfa71ce749262ec1
DemiGod <demigod14100@gmail.com>
2025-02-08 22:37:19 +0530

    Fixed-Button

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -44,16 +44,9 @@ const RecentProjects = () => {
                 as={Link}
                 href="https://github.com/Demigod1410/zeniski"
                 target="__blank"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                className="px-4 py-2 rounded-xl text font-normal dark:text-white"
               >
                 View Project →
-              </CardItem>
-              <CardItem
-                translateZ={20}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-              >
-                Sign up
               </CardItem>
             </div>
           </CardBody>
@@ -89,16 +82,9 @@ const RecentProjects = () => {
                 as={Link}
                 href="https://github.com/nandiniiiiiiii/CareerCall"
                 target="__blank"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                className="px-4 py-2 rounded-xl text font-normal dark:text-white"
               >
                 View Project →
-              </CardItem>
-              <CardItem
-                translateZ={20}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-              >
-                Sign up
               </CardItem>
             </div>
           </CardBody>
@@ -128,16 +114,9 @@ const RecentProjects = () => {
                 as={Link}
                 href="https://github.com/Demigod1410/SpaceLord"
                 target="__blank"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                className="px-4 py-2 rounded-xl text font-normal dark:text-white"
               >
                 View Project →
-              </CardItem>
-              <CardItem
-                translateZ={20}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-              >
-                Sign up
               </CardItem>
             </div>
           </CardBody>
